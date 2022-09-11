acd310e0beb130c8609810e96f6d3e57689b96ca
hooope81 <hooope81@gmail.com>
2022-09-11 17:38:47 +0300

    lesson3

---
+++ tests/HelloTest.php
@@ -0,0 +1,11 @@
+<?php
+namespace GeekBrains\LevelTwo;
+use PHPUnit\Framework\TestCase;
+
+class HelloTest extends TestCase
+{
+    public function testItWorks(): void
+    {
+        $this->assertTrue(true);
+    }
+}
